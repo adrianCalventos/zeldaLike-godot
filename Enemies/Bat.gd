588a5b4dbebe57f085eaab28d3b2dadc5c709774
adriancalventos <adriancalventosgarcia@gmail.com>
2023-11-16 01:17:27 +0100

    setget

--- Enemies/Bat.gd
+++ Enemies/Bat.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var stats = $Stats;
 
 func _physics_process(delta):
 	velocity = velocity.move_toward(Vector2.ZERO, 200*delta);
@@ -7,5 +8,9 @@ func _physics_process(delta):
 
 
 func _on_hurt_box_area_entered(area):
-	velocity =  Vector2.RIGHT * 200;
+	stats.health-=1;
+	if (stats.health<=0):
+		queue_free();
+	var knockback_vector = area.knockback_vector;
+	velocity =  knockback_vector * 120;
 	##queue_free();
